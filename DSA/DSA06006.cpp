1ae2651f27ee4e63194a3047fea458587420b63c
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-08-20 21:34:28 +0700

    Add files via upload

---
+++ DSA/DSA06006.cpp
@@ -0,0 +1,48 @@
+// LonggVuz.
+#include<bits/stdc++.h>
+using namespace std;
+// Noob C++
+void End(){
+    cerr << "=> Thời gian code chạy: ";
+    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+}
+#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define fix(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define ms(a, x) memset(a, x, sizeof(a))
+#define tc() int TC; cin >> TC; while(TC--)
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define str string
+#define int intmax_t
+#define db double
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int maxn = 1e6 + 7;
+
+
+
+signed main(){
+	LonggVuz();
+	
+	tc(){
+		int n; cin >> n;
+		int a[n+5], dem[5] = {0};
+		for(int i=1; i<=n; i++){
+			cin >> a[i];
+			++dem[a[i]];
+		}
+		for(int i=0; i<=2; i++){
+			while(dem[i]--){
+				cout << i << ' ';
+			}
+		}cout << el;
+	}
+	
+	End();
+}
