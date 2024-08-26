154693e037fda9ac2126c0e2294ca86a838653f6
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-08-26 20:15:36 +0700

    Add files via upload

---
+++ DSA/DSAKT055.cpp
@@ -0,0 +1,53 @@
+// LonggVuz.
+#include<bits/stdc++.h>
+using namespace std;
+// Noob C++
+void End(){
+    cerr << "=> Thời gian code chạy: ";
+    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+}
+#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define fix(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define ms(a, x) memset(a, x, sizeof(a))
+#define tc() int TC; cin >> TC; while(TC--)
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define str string
+#define int intmax_t
+#define ld long double
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int maxn = 1e6 + 7;
+
+int f[1005];
+
+signed main(){
+	LonggVuz();
+	
+	tc(){
+		ms(f, 0);
+		int n, s; cin >> n >> s;
+		int a[n+5], c[n+5];
+		for(int i=1; i<=n; i++){
+			cin >> a[i];
+		}
+		for(int i=1; i<=n; i++){
+			cin >> c[i];
+		}
+		for(int i=1; i<=n; i++){
+			for(int j=s; j>=a[i]; j--){
+				if(f[j - a[i]] != 0 || j == a[i]){
+					f[j] = max(f[j], f[j - a[i]] + c[i]);
+				}
+			}
+		}cout << f[s] << el;
+	}
+
+	End();
+}
