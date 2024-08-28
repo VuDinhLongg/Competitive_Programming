632c4a29d593362dd2179a2c56de0f8d0916fe7d
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-08-28 21:41:14 +0700

    Add files via upload

---
+++ DSA/DSA04013.cpp
@@ -0,0 +1,56 @@
+// LonggVuz.
+#include<bits/stdc++.h>
+using namespace std;
+// Noob C++
+void End(){
+    cerr << "=> Thời gian code chạy: ";
+    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+}
+#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define fix(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define ms(a, x) memset(a, x, sizeof(a))
+#define tc() int TC; cin >> TC; while(TC--)
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define str string
+#define int intmax_t
+#define ld long double
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int maxn = 1e5 + 7;
+
+int mark[maxn];
+void solve(int a[], int n){
+	for(int i=1; i<=n; i++) mark[i] = 0;
+	sort(a+1, a+n+1);
+	int cnt = 1, j = n / 2 + 1;
+	for(int i=1; i<n/2+1; i++){
+		if(a[i] * 2 <= a[j]){
+			mark[i] = mark[j] = cnt; ++cnt;
+			++j; if(j > n) break;
+		}else{
+			--i; ++j; if(j > n) break;
+		}
+	}cout << n - cnt + 1 << el;
+	//for(int i=1; i<=n; i++) cout << mark[i] << ' '; cout << el;
+}
+
+signed main(){
+	LonggVuz();
+	
+	tc(){
+		int n; cin >> n;
+		int a[n+5];
+		for(int i=1; i<=n; i++){
+			cin >> a[i];
+		}solve(a, n);
+	}
+	
+	End();
+}
