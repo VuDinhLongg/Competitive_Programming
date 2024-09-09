c87370eb114edb7462808d32b3bbd33c5e29dd38
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-09-10 00:25:04 +0700

    Add files via upload

---
+++ DSA/DSA02021.cpp
@@ -0,0 +1,45 @@
+// LonggVuz.
+#include<bits/stdc++.h>
+using namespace std;
+// Noob C++
+void End(){
+    cerr << "=> Thời gian code chạy: ";
+    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+}
+#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define fix(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define ms(x) memset(x, 0, sizeof(x))
+#define tc() int TC; cin >> TC; while(TC--)
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define str string
+#define int int64_t
+#define ld long double
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int maxn = 1e6 + 7;
+
+
+
+signed main(){
+	LonggVuz();
+	
+	tc(){
+		str a; int k; cin >> a >> k;
+		int n = len(a);
+		while(k > n){
+			int tmp = n;
+			while(tmp * 2 < k) tmp *= 2;
+			k -= (tmp + 1);
+			if(k == 0) k = tmp;
+		}cout << a[k - 1] << el;
+	}
+	
+	End();
+}
