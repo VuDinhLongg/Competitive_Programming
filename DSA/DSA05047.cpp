00525da2ff9cb3e3b5113debd21339aa327df564
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-09-17 20:59:30 +0700

    Update DSA05047.cpp

--- DSA/DSA05047.cpp
+++ DSA/DSA05047.cpp
@@ -6,13 +6,12 @@ void End(){
     cerr << "=> Thời gian code chạy: ";
     cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
 }
-#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define Sonic() ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
 #define fix(x) fixed << setprecision(x)
 #define all(x) x.begin(), x.end()
 #define len(x) (int)x.size()
 #define ms(x) memset(x, 0, sizeof(x))
-#define tc() int TC; cin >> TC; while(TC--)
 #define el '\n'
 #define fi first
 #define se second
@@ -26,58 +25,59 @@ const int oo = 1e18 + 7;
 const int maxn = 1e4 + 7;
 
 int n, a[5][maxn], f[maxn][20], d[20];
-// f[i][j] : tong lon nhat khi xet den cot i o trang thai j
+// f[i][j] : tổng lớn nhất khi xét đến cột i ở trạng thái j
+// d[i] = 1 -> trạng thái i là thoả mãn, ngược lại là ko thoả mãn
 bool bit(int n, int i){
-	return (n >> i) & 1; // bit thu i cua n
+	return (n >> i) & 1; // bit thứ i của số n
 }
 
 void hehe(){
 	for(int i=0; i<15; i++){
 		bool ok = 1;
 		for(int j=0; j<3; j++){
-			if(bit(i, j) && bit(i, j + 1)) ok = 0;
+			if(bit(i, j) && bit(i, j + 1)){
+				ok = 0; break;
+			}
 		}d[i] = ok;
 	}
 }
 
-void solve(){
+void LonggVuz(){
+	int mx = -oo;
+	cin >> n;
+	for(int i=0; i<4; i++){
+		for(int j=1; j<=n; j++){
+			cin >> a[i][j];
+			mx = max(mx, a[i][j]);
+		}
+	}if(mx <= 0) return cout << mx << el, void();
 	ms(f);
 	for(int i=1; i<=n; i++){
-		for(int mask=0; mask<16; mask++){
+		for(int mask=0; mask<15; mask++){
 			if(d[mask]){
 				int sum = 0;
 				for(int j=0; j<4; j++){
 					if(bit(mask, j)) sum += a[j][i];
-				}
-				for(int pre_mask=0; pre_mask<16; pre_mask++){
-					if(d[pre_mask] && ((pre_mask & mask) == 0)){
+				}f[i][mask] = -oo;
+				for(int pre_mask=0; pre_mask<15; pre_mask++){
+					if(d[pre_mask] && (mask & pre_mask) == 0){
 						f[i][mask] = max(f[i][mask], f[i-1][pre_mask] + sum);
 					}
 				}
 			}
 		}
 	}int res = -oo;
-	for(int i=0; i<16; i++){
+	for(int i=0; i<15; i++){
 		if(d[i]) res = max(res, f[n][i]);
 	}cout << res << el;
 }
 
 signed main(){
-	LonggVuz();
+	Sonic();
 	
 	hehe();
-	tc(){
-		cin >> n;
-		int res = -oo;
-		for(int i=0; i<4; i++){
-			for(int j=1; j<=n; j++){
-				cin >> a[i][j];
-				res = max(res, a[i][j]);
-			}
-		}if(res < 0){
-			cout << res << el;
-		}else solve();
-	}
+	int TC = 1; cin >> TC;
+	while(TC--) LonggVuz();
 	
 	End();
 }
