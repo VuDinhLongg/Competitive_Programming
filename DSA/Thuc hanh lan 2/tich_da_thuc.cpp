2938b33163b5692ee07824b0322fcbfe25940fb8
Vũ Đình Longg <160626269+VuDinhLongg@users.noreply.github.com>
2024-09-21 12:37:20 +0700

    Create tich_da_thuc.cpp

---
+++ DSA/Thuc hanh lan 2/tich_da_thuc.cpp
@@ -0,0 +1,53 @@
+// LonggVuz.
+#include<bits/stdc++.h>
+using namespace std;
+// Legendary Grandmaster
+void End(){
+    cerr << "=> Thời gian code chạy: ";
+    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+}
+#define Sonic() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define fix(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define ms(a, x) memset(a, x, sizeof(a))
+#define bit(n, x) (n >> x & 1)
+#define el '\n'
+#define ld long double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int maxn = 1e6 + 7;
+
+int f[205];
+
+void LonggVuz(){
+	ms(f, 0);
+	int n, m; cin >> n >> m;
+	int a[n+5], b[m+5];
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	for(int i=0; i<m; i++){
+		cin >> b[i];
+	}
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			f[i + j] += a[i] * b[j];
+		}
+	}int x = n + m - 1;
+	for(int i=0; i<x; i++){
+		cout << f[i] << ' ';
+	}cout << el;
+}
+
+signed main(){
+	Sonic();
+	
+	int TC = 1; cin >> TC;
+	while(TC--) LonggVuz();
+	
+	End();
+}
