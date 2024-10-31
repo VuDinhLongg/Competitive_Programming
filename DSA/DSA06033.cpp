b51ee630746e7f1b8f01923bc29f76b64b91a560
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2024-10-31 23:17:01 +0700

    Update DSA06033.cpp

--- DSA/DSA06033.cpp
+++ DSA/DSA06033.cpp
@@ -1,56 +1,55 @@
-// LonggVuz.
+// LonggVuz
 #include<bits/stdc++.h>
 using namespace std;
-// Noob C++
+// Think twice, code once ^.^
 void End(){
-    cerr << "=> Thời gian code chạy: ";
-    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
+    cerr << "=> Smoothly Finished! Time taken: ";
+    cerr << (0.001 * clock()) << "s" << string(25, '\t');
 }
-#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
+#define Sonic() cin.tie(NULL) -> sync_with_stdio(false);
 #define fix(x) fixed << setprecision(x)
-#define all(x) x.begin(), x.end()
+#define all(x) begin(x), end(x)
 #define len(x) (int)x.size()
+#define gel(x) getline(cin >> ws, x)
 #define ms(a, x) memset(a, x, sizeof(a))
-#define tc() int TC; cin >> TC; while(TC--)
-#define el '\n'
-#define fi first
-#define se second
+#define bit(n, x) (n >> x & 1)
 #define pb push_back
-#define str string
-#define int intmax_t
-#define db double
-#define pii pair<int, int>
+#define is insert
+#define el '\n'
+#define ld long double
+#define int int64_t
 
 const int mod = 1e9 + 7;
 const int oo = 1e18 + 7;
-const int maxn = 1e5 + 7;
+const int mxn = 1e5 + 7;
 
-int f[maxn];
+int mx[mxn];
 
-void solve(int a[], int n){
-	int res = -1;
-	f[n] = a[n];
-	for(int i=n-1; i>=1; i--) f[i] = max(f[i + 1], a[i]);
-	int i = 1, j = 1;
-	while(i <= n && j <= n){
-		if(a[i] < f[j]){
-			res = max(res, j - i);
-			++j;
-		}else ++i;
-	}cout << res << el;
+void LonggVuz(){
+    int n; cin >> n;
+    int a[n+5];
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+    }
+    mx[n] = a[n];
+    for(int i=n-1; i>=1; i--){
+        mx[i] = max(mx[i + 1], a[i]);
+    }
+    int res = 0, j = 2;
+    for(int i=1; i<=n; i++){
+        while(j <= n and mx[j] > a[i]) ++j;
+        res = max(res, j - i - 1);
+        if(j > n) break;
+    }
+    if(res) cout << res << el;
+    else cout << "-1\n";
 }
 
 signed main(){
-	LonggVuz();
-	
-	tc(){
-		int n; cin >> n;
-		int a[n+5];
-		for(int i=1; i<=n; i++){
-			cin >> a[i];
-		}solve(a, n);
-	}
-	
-	End();
+    Sonic();
+    
+    int TC = 1; cin >> TC;
+    while(TC--) LonggVuz();
+    
+    End();
 }
