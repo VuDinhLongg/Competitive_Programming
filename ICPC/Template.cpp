5edd1df8d259953e7a62849208fb4ba804ec5e00
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-02-12 22:45:22 +0700

    Create Template.cpp

---
+++ ICPC/Template.cpp
@@ -0,0 +1,34 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+// ☞ Think twice, solve once ☜
+#define fo(i, n) for(int i=1; i<=n; i++)
+#define fix(x) fixed << setprecision(x)
+#define out(x) return cout << x, void()
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define el '\n'
+#define db double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e16 + 8;
+const int mxn = 1e6 + 9;
+
+
+
+void LonggVuz(){
+    
+}
+
+signed main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    
+    int tc = 1; //cin >> tc;
+    while(tc--){
+        LonggVuz();
+        if(tc) cout << el;
+    }
+    
+    cerr << "☞ Execution Time: " << 0.001 * clock() << "s ☜\n";
+}
