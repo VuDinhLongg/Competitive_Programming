35264803a4c087854efee45c751c0d3cc9cc6557
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-02-12 22:37:26 +0700

    Delete Python directory

--- Python/py01037.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from math import *
-from collections import *
-
-if os.path.exists('Accepted.py'):
-    sys.stdin = open('icpc.inp', 'r')
-    sys.stdout = open('icpc.out', 'w')
-
