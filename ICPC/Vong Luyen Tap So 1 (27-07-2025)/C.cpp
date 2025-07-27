9c671464dcaa679abca7aad071d268cc92f12253
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-07-27 08:03:58 +0700

    Update C.cpp

--- ICPC/Vong Luyen Tap So 1 (27-07-2025)/C.cpp
+++ ICPC/Vong Luyen Tap So 1 (27-07-2025)/C.cpp
@@ -1,1 +1,48 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pb push_back
+#define po pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 8;
+const int mxn = 1e6 + 9;
+
+
+
+void LonggVuz(){
+    string s; cin >> s;
+    int n = len(s);
+    vec<int> a, z;
+    s.insert(begin(s), ' ');
+    fo(i, 1, n){
+        if(s[i] == 'A') a.pb(i);
+        if(s[i] == 'Z') z.pb(i);
+    }
+    if(len(a) == 0 or len(z) == 0) cout << 0;
+    else cout << z.back() - a[0] + 1;
+}
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int o = 1; //cin >> o;
+    while(o){
+        LonggVuz();
+        if(--o) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+    return (0 ^ 0);
+}
