adc9e1a279acbf42466ed9d264dd148e84980baa
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-07-27 07:47:50 +0700

    Update Template.cpp

--- ICPC/Template.cpp
+++ ICPC/Template.cpp
@@ -1,21 +1,22 @@
 // LonggVuz
 #include<bits/stdc++.h>
 using namespace std;
-// ☞ Voldemort ☜
 #define el cout << '\n'
-#define fo(i, a, b) for(int i=a; i<=b; i++)
-#define fd(i, a, b) for(int i=a; i>=b; i--)
-#define out(x) return cout << x, void()
-#define all(x) x.begin(), x.end()
-#define len(x) (int)x.size()
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
 #define vec vector
 #define pb push_back
 #define po pop_back
 #define int int64_t
 
 const int mod = 1e9 + 7;
-const int oo = 8e18 + 8;
-const int mxn = 1e6 + 5;
+const int oo = 1e18 + 8;
+const int mxn = 1e6 + 9;
 
 
 
@@ -24,7 +25,7 @@ void LonggVuz(){
 }
 
 signed main(){
-    ios_base::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     
     int o = 1; //cin >> o;
@@ -34,4 +35,5 @@ signed main(){
     }
     
     cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+    return (0 ^ 0);
 }
