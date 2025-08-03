c4d9620771b83597f6bf55f22ef1b9bf03980b3a
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-03 08:07:31 +0700

    Update B.cpp

--- ICPC/Vong Luyen Tap So 2 (03-08-2025)/B.cpp
+++ ICPC/Vong Luyen Tap So 2 (03-08-2025)/B.cpp
@@ -1,1 +1,50 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pb push_back
+#define po pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 8;
+const int mxn = 1e6 + 9;
+
+
+
+void LonggVuz(){
+    int n; cin >> n;
+    deque<int> q;
+    int ok = 1;
+    fo(i, 1, n){
+        int x; cin >> x;
+        if(ok) q.pb(x);
+        else q.push_front(x);
+        ok ^= 1;
+    }
+    if(ok){
+        fo(i, 1, n) cout << q[i - 1] << ' ';
+    }else{
+        fd(i, n, 1) cout << q[i - 1] << ' ';
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz--){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
