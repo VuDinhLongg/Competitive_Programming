ff4830f9ced2a7ec8a386f11f244791e0783b0fa
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-08 22:36:19 +0700

    Update DSA05019.cpp

--- DSA/DSA05019.cpp
+++ DSA/DSA05019.cpp
@@ -1,62 +1,68 @@
-// LonggVuz.
+// LonggVuz
 #include<bits/stdc++.h>
 using namespace std;
-// Noob C++
-void End(){
-    cerr << "=> Thời gian code chạy: ";
-    cerr << (1.0 * clock() / CLOCKS_PER_SEC) << " giây" << string(27, '\t');
-}
-#define LonggVuz() ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define use(x) freopen(x".inp", "r", stdin); freopen(x".out", "w", stdout);
-#define fix(x) fixed << setprecision(x)
-#define all(x) x.begin(), x.end()
-#define len(x) (int)x.size()
-#define ms(a, x) memset(a, x, sizeof(a))
-#define tc() int TC; cin >> TC; while(TC--)
-#define el '\n'
-#define fi first
-#define se second
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
 #define pb push_back
-#define str string
-#define int intmax_t
-#define ld long double
+#define po pop_back
+#define int int64_t
 
 const int mod = 1e9 + 7;
-const int oo = 1e18 + 7;
-const int maxn = 1e6 + 7;
+const int oo = 1e18 + 8;
+const int mxn = 1e6 + 9;
 
-int n, m, a[505][505], f[505][505];
+int n, m, a[1001][1001], dp[1001][1001];
 
-void solve(){
-	int res = 0;
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=m; j++){
-			if(a[i][j]){
-				int mi = oo;
-				for(int k=j; k<=m; k++){
-					if(f[i][j] == 0) break;
-					mi = min(mi, f[i][k]);
-					int x = min(mi, k - j + 1);
-					res = max(res, x);
-				}
-			}
-		}
-	}cout << res << el;
+void LonggVuz(){
+    cin >> n >> m;
+    fo(i, 1, n) fo(j, 1, m){
+        cin >> a[i][j];
+        if(a[i][j]) dp[i][j] = dp[i - 1][j] + 1;
+    }
+    int res = 0;
+    fo(i, 1, n){
+        vec<int> l(m + 1), r(m + 1);
+        {
+            stack<int> st;
+            fo(j, 1, m){
+                while(!st.empty() and dp[i][st.top()] >= dp[i][j]) st.pop();
+                if(st.empty()) l[j] = 0;
+                else l[j] = st.top();
+                st.push(j);
+            }
+        }
+        {
+            stack<int> st;
+            fd(j, m, 1){
+                while(!st.empty() and dp[i][st.top()] >= dp[i][j]) st.pop();
+                if(st.empty()) r[j] = m + 1;
+                else r[j] = st.top();
+                st.push(j);
+            }
+        }
+        fo(j, 1, m) res = max(res, min(dp[i][j], r[j] - l[j] - 1));
+    }
+    cout << res;
+    fo(i, 1, n) fo(j, 1, m) dp[i][j] = 0;
 }
 
 signed main(){
-	LonggVuz();
-	
-	tc(){
-		cin >> n >> m;
-		for(int i=1; i<=n; i++){
-			for(int j=1; j<=m; j++){
-				cin >> a[i][j];
-				if(a[i][j]) f[i][j] = f[i-1][j] + 1;
-				else f[i][j] = 0;
-			}
-		}solve();
-	}
-	
-	End();
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int o = 1; cin >> o;
+    while(o){
+        LonggVuz();
+        if(--o) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+    return (0 ^ 0);
 }
