bcfd4986c7749369c4aeba4c1286837d8969c3c0
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-10 08:19:25 +0700

    Update A.cpp

--- ICPC/Vong Luyen Tap So 3 (10-08-2025)/A.cpp
+++ ICPC/Vong Luyen Tap So 3 (10-08-2025)/A.cpp
@@ -1,1 +1,43 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int k, s; cin >> k >> s;
+    int res = 0;
+    fo(x, 0, k) fo(y, 0, k){
+        int z = s - x - y;
+        if(0 <= z and z <= k) ++res;
+    }
+    cout << res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
