848ac791870182c012a568ff7865c7b5acb3368e
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-10 09:58:49 +0700

    Update E.cpp

--- ICPC/Vong Luyen Tap So 3 (10-08-2025)/E.cpp
+++ ICPC/Vong Luyen Tap So 3 (10-08-2025)/E.cpp
@@ -1,1 +1,38 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+string a = "Alice", b = "Bob";
+
+void LonggVuz(){
+    int x, y; cin >> x >> y;
+    cout << (abs(x - y) <= 1 ? b : a);
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
