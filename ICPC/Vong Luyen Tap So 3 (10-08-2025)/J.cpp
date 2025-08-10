f48064ba3393042e168b5aa0d2ece9a21f12cc33
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-10 08:19:54 +0700

    Update J.cpp

--- ICPC/Vong Luyen Tap So 3 (10-08-2025)/J.cpp
+++ ICPC/Vong Luyen Tap So 3 (10-08-2025)/J.cpp
@@ -1,1 +1,51 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int n, c; cin >> n >> c;
+    int a[n+5];
+    fo(i, 1, n) cin >> a[i];
+    sort(a+1, a+n+1);
+    multiset<int> res;
+    int ans = 0;
+    fd(i, n, 1){
+        auto it = res.upper_bound(c - a[i]);
+        if(it == res.begin()) res.insert(a[i]), ++ans;
+        else{
+            --it;
+            res.erase(it);
+        }
+    }
+    cout << ans;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
