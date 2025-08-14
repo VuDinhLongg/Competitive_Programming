825d63fe5c1558b2e9d71cb041fd5f749974691c
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-14 22:26:07 +0700

    Create DSA_P136.cpp

---
+++ DSA/DSA_P136.cpp
@@ -0,0 +1,62 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    string s; cin >> s;
+    stack<char> l, r;
+    for(char &c : s){
+        if(c == '<'){
+            if(l.empty()) continue;
+            char i = l.top(); l.pop();
+            r.push(i);
+        }else if(c == '>'){
+            if(r.empty()) continue;
+            char i = r.top(); r.pop();
+            l.push(i);
+        }else if(c == '-'){
+            if(!l.empty()) l.pop();
+        }else{
+            l.push(c);
+        }
+    }
+    string res = "";
+    while(!l.empty()){
+        res.pub(l.top()); l.pop();
+    }
+    reverse(all(res));
+    cout << res;
+    while(!r.empty()){
+        cout << r.top(); r.pop();
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
