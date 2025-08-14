cdba2183f4c383ecb19da14e79e39e4728139439
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-14 23:22:43 +0700

    Create DSA_P088.cpp

---
+++ DSA/DSA_P088.cpp
@@ -0,0 +1,52 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    string s; getline(cin >> ws, s);
+    stack<char> st;
+    for(char &c : s){
+        if(c == '('){
+            st.push(c);
+        }else if(c == '['){
+            st.push(c);
+        }else if(c == ')'){
+            if(st.empty() or st.top() != '(') out("NO");
+            st.pop();
+        }else if(c == ']'){
+            if(st.empty() or st.top() != '[') out("NO");
+            st.pop();
+        }
+    }
+    cout << (st.empty() ? "YES" : "NO");
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
