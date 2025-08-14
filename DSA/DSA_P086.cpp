9131224d9eb770d378be33a7d0fd9d1c310c5e2c
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-14 23:15:15 +0700

    Create DSA_P086.cpp

---
+++ DSA/DSA_P086.cpp
@@ -0,0 +1,46 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int n, q; cin >> n >> q;
+    int a[n+5] = {};
+    while(q--){
+        int l, r; cin >> l >> r;
+        ++a[l]; --a[r + 1];
+    }
+    fo(i, 1, n){
+        a[i] += a[i - 1];
+        cout << a[i] % 2 << ' ';
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
