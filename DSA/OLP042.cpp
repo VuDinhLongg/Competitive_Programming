decf2e8b540374de0688515c9e8cfd8d2197adf0
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-19 15:10:39 +0700

    Create OLP042.cpp

---
+++ DSA/OLP042.cpp
@@ -0,0 +1,43 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+int cnt[mxn];
+void pre(){
+    fo(i, 1, 1e6){
+        inc(j, i, 1e6, i) ++cnt[j];
+    }
+}
+
+void LonggVuz(){
+    int n; cin >> n;
+    cout << cnt[n];
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    pre();
+    signed orz = 1; cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
