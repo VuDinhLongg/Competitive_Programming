fa276cc93b54fe5c2cac94a87bfe64bd05aab5bb
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-23 22:34:29 +0700

    Create OLP22T10.cpp

---
+++ DSA/OLP22T10.cpp
@@ -0,0 +1,54 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e5 + 7;
+
+int n, a[mxn], pf[mxn];
+
+void LonggVuz(){
+    cin >> n;
+    fo(i, 1, n){
+        cin >> a[i];
+        pf[i] = pf[i - 1] + a[i];
+    }
+    int sum = 0, len = 1;
+    vec<int> res;
+    fo(i, 1, n - 2){
+        if((pf[n] - pf[i]) * len > sum * (n - i)){
+            res.clear();
+            res.pub(i);
+            sum = pf[n] - pf[i];
+            len = n - i;
+        }else if((pf[n] - pf[i]) * len == sum * (n - i)){
+            res.pub(i);
+        }
+    }
+    for(int &i : res) cout << i << ' ';
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
