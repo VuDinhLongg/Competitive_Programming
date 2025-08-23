6f11a2fcf51260e975c39d39a3e9c68a97ed17c0
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-23 20:17:53 +0700

    Update I.cpp

--- ICPC/Vong Luyen Tap So 2 (03-08-2025)/I.cpp
+++ ICPC/Vong Luyen Tap So 2 (03-08-2025)/I.cpp
@@ -4,61 +4,57 @@ using namespace std;
 #define el cout << '\n'
 #define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
 #define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
-#define fo(i, a, b) for(int64_t i=a; i<=b; i++)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
 #define fd(i, a, b) for(int32_t i=a; i>=b; i--)
 #define out(x) return void(cout << (x));
 #define all(x) begin(x), end(x)
 #define len(x) (int)(x).size()
 #define vec vector
-#define pb push_back
-#define po pop_back
-#define int uint64_t
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
 const int mod = 1e9 + 7;
-const int oo = 1e18 + 8;
-const int mxn = 1e6 + 9;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
 
-string res;
 int c[66][66];
-
-void run(int a, int b, int k){
-    if(a == 0 and b == 0) return;
-    if(a and !b){
-        res.pb('a'); --a;
-        run(a, b, k); return;
-    }
-    if(b and !a){
-        res.pb('b'); --b;
-        run(a, b, k); return;
-    }
-    int n = c[a + b - 1][a - 1];
-    if(k > n){
-        k -= n;
-        res.pb('b');
-        --b;
-        run(a, b, k);
-    }else{
-        res.pb('a');
-        --a;
-        run(a, b, k);
+void pre(){
+    fo(i, 0, 60){
+        fo(j, 0, i){
+            if(j == 0 or j == i) c[i][j] = 1;
+            else c[i][j] = c[i - 1][j] + c[i - 1][j - 1];
+        }
     }
 }
 
 void LonggVuz(){
-    fo(i, 0, 60) fo(j, 0, i){
-        if(j == 0 or j == i) c[i][j] = 1;
-        else c[i][j] = c[i - 1][j] + c[i - 1][j - 1];
-    }
+    pre();
     int a, b, k; cin >> a >> b >> k;
-    run(a, b, k);
-    cout << res;
+    while(a or b){
+        if(a >= 1){
+            int cnt = c[a - 1 + b][a - 1];
+            if(k <= cnt){
+                cout << 'a';
+                --a;
+            }else{
+                cout << 'b';
+                --b;
+                k -= cnt;
+            }
+        }else{
+            cout << 'b';
+            --b;
+            k -= 1;
+        }
+    }
 }
 
 signed main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
     
     signed orz = 1; //cin >> orz;
-    while(orz--){
+    while(orz --> 0){
         LonggVuz();
         if(orz) el;
     }
