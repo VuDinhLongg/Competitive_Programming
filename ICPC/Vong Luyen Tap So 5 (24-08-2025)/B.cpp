c564cccd62dcdb2c4f3c5ecf04b9a40925980204
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-24 08:25:00 +0700

    Update B.cpp

--- ICPC/Vong Luyen Tap So 5 (24-08-2025)/B.cpp
+++ ICPC/Vong Luyen Tap So 5 (24-08-2025)/B.cpp
@@ -1,1 +1,53 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    string s; cin >> s;
+    int n = len(s), id = 0;
+    while(id < n and s[id] != '*') ++id;
+    int res = 0;
+    stack<int> st;
+    fo(i, 0, n - 1) if(i != id){
+        if(s[i] == '('){
+            st.push(i);
+        }else{
+            if(st.empty()) continue;
+            if(st.top() < id and id < i){
+                ++res;
+            }
+            st.pop();
+        }
+    }
+    cout << res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
