cc39f590e7d84b4c292e799af706739b6b6707e7
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-08-24 08:27:18 +0700

    Update D.cpp

--- ICPC/Vong Luyen Tap So 5 (24-08-2025)/D.cpp
+++ ICPC/Vong Luyen Tap So 5 (24-08-2025)/D.cpp
@@ -1,1 +1,49 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int n; cin >> n;
+    vec<string> v(n);
+    for(auto &i : v) cin >> i;
+    sort(all(v), [&](string x, string y){
+        if(len(x) == len(y)) return x > y;
+        return len(x) > len(y);
+    });
+    vec<string> res;
+    fo(i, 0, 2) res.pub(v[i]);
+    sort(all(res), [&](string x, string y){
+        return x + y > y + x;
+    });
+    for(auto &i : res) cout << i;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
