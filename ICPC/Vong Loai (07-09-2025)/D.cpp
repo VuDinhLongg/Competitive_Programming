456d73e85ddd354481d780076aff3bde3143db7c
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-09-09 13:56:06 +0700

    Update D.cpp

--- ICPC/Vong Loai (07-09-2025)/D.cpp
+++ ICPC/Vong Loai (07-09-2025)/D.cpp
@@ -1,1 +1,48 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define int int64_t
 
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int n; cin >> n;
+    string a; cin >> a;
+    a.insert(begin(a), ' ');
+    int res = 0, i = 1;
+    while(i <= n){
+        int j = i + 1;
+        while(j <= n and a[i] == a[j]) ++j;
+        int l = j - i;
+        res += l * (l - 1) / 2;
+        i = j;
+    }
+    cout << res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; //cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+}
