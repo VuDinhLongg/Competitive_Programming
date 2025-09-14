747223d7b51dfb3d35d79a006de881e510939bb9
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-09-14 13:41:35 +0700

    Create B.cpp

---
+++ ICPC/Vong Luyen Tap So 7 (14-09-2025)/B.cpp
@@ -0,0 +1,52 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include<LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x));
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define float double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+void LonggVuz(){
+    int n; cin >> n;
+    int a[n+5];
+    fo(i, 1, n) cin >> a[i];
+    sort(a+1, a+n+1);
+    fo(i, 1, n - 1) if(a[i] + 1 < a[i + 1]){
+        cout << a[i] + 1;
+        return;
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
