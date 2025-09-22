7bbbdcce7e2a26fa745623dbb6265af0df93f8dc
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-09-23 02:11:51 +0700

    Create C.cpp

---
+++ ICPC/Vong Chung Ket (21-09-2025)/C.cpp
@@ -0,0 +1,55 @@
+// LonggVuz
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include<LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x))
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define float double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+bool dx(int n){
+    string s = to_string(n);
+    string t = s;
+    reverse(all(t));
+    return s == t;
+}
+
+void LonggVuz(){
+    int n; cin >> n;
+    int res = 0;
+    for(int i=1; i*i*i<=n; i++){
+        if(dx(i * i * i)) res = i;
+    }
+    cout << res * res * res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
