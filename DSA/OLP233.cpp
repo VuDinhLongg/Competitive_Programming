513cc70baef0a6ae2de1c65390e89c712d8de32f
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-10-02 21:42:15 +0700

    Create OLP233.cpp

---
+++ DSA/OLP233.cpp
@@ -0,0 +1,54 @@
+// LonggVuz
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x))
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define dub double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+inline void LonggVuz(){
+    int n; cin >> n;
+    int a[n+5];
+    fo(i, 1, n) cin >> a[i];
+    sort(a+1, a+n+1);
+    int res = 0, cur = 0;
+    fo(i, 1, n){
+        res += a[i] * (i - 1) - cur;
+        cur += a[i];
+    }
+    cout << res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
