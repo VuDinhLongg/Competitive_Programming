63b3ff8c7279aec8fc2b00e7d474fc837d83d49d
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-11-01 20:06:09 +0700

    Create OLP259.cpp

---
+++ DSA/OLP/OLP259.cpp
@@ -0,0 +1,58 @@
+// LonggVuz
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LonggVuz
+#include <LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+template<class X, class Y> bool maxi(X &a, const Y &b){ return b > a ? a = b, 1 : 0; }
+template<class X, class Y> bool mini(X &a, const Y &b){ return b < a ? a = b, 1 : 0; }
+
+#define el cout << '\n'
+#define fup(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define fdn(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x))
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define dub double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 5e3 + 7;
+
+inline void LonggVuz(){
+    string a, b; cin >> a >> b;
+    int n = len(a);
+    a = " " + a;
+    b = " " + b;
+    int res = 0;
+    fo(i, 1, n){
+        int j = i;
+        fo(k, 1, n){
+            if(j <= n and b[k] == a[j]) ++j;
+        }
+        maxi(res, j - i);
+    }
+    cout << n - res;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
