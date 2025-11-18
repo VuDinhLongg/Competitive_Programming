97e9b384861443af4c85c5a3a7f1e9a8148d9141
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2025-11-18 14:16:52 +0700

    Update OLP200.cpp

--- DSA/OLP/OLP200.cpp
+++ DSA/OLP/OLP200.cpp
@@ -1,99 +1,91 @@
 // LonggVuz
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LonggVuz
+#include <LonggVuz.h>
+#else
+#define debug(...)
+#define print(...)
+#endif
+
+template<class X, class Y> bool maxi(X &a, const Y &b){ return b > a ? a = b, 1 : 0; }
+template<class X, class Y> bool mini(X &a, const Y &b){ return b < a ? a = b, 1 : 0; }
+
 #define el cout << '\n'
-#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
-#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fup(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define fdn(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
 #define fo(i, a, b) for(int32_t i=a; i<=b; i++)
 #define fd(i, a, b) for(int32_t i=a; i>=b; i--)
-#define out(x) return void(cout << (x));
+#define out(x) return void(cout << (x))
 #define all(x) begin(x), end(x)
 #define len(x) (int)(x).size()
 #define vec vector
 #define pub push_back
 #define pob pop_back
+#define dub double
 #define int int64_t
 
 const int mod = 1e9 + 7;
 const int oo = 1e18 + 7;
 const int mxn = 1e6 + 7;
 
+#define pii pair<int, int>
+#define pip pair<int, pii>
+#define ppi pair<pii, int>
+#define fi first
+#define se second
 
+int cal1(pii a[], int n){
+    sort(a+1, a+n+1, [&](pii &x, pii &y){
+        if(x.fi == y.fi) return x.se < y.se;
+        return x.fi < y.fi;
+    });
+    auto get = [&](int id){
+        int ans = 0;
+        fo(i, 1, n) ans += abs(a[i].fi - a[id].fi) + abs(a[i].se - a[id].se);
+        ans -= (id - 1 + 1) * (id - 1) / 2;
+        ans -= (n - id + 1) * (n - id) / 2;
+        return ans;
+    };
+    int mid = (n + 1) / 2;
+    return min(get(mid), get(mid + 1));
+}
 
-void LonggVuz(){
+int cal2(pii a[], int n){
+    sort(a+1, a+n+1, [&](pii &x, pii &y){
+        if(x.se == y.se) return x.fi < y.fi;
+        return x.se < y.se;
+    });
+    auto get = [&](int id){
+        int ans = 0;
+        fo(i, 1, n) ans += abs(a[i].fi - a[id].fi) + abs(a[i].se - a[id].se);
+        ans -= (id - 1 + 1) * (id - 1) / 2;
+        ans -= (n - id + 1) * (n - id) / 2;
+        return ans;
+    };
+    int mid = (n + 1) / 2;
+    return min(get(mid), get(mid + 1));
+}
+
+inline void LonggVuz(){
     int n; cin >> n;
-    int a[n+5], b[n+5];
+    pii a[n+5], b[n+5];
     fo(i, 1, n){
-        cin >> a[i] >> b[i];
+        cin >> a[i].fi >> a[i].se;
+        b[i] = a[i];
     }
-    map<int, int> cntx, cnty;
-    vec<int> mx, my;
-    fo(i, 1, n){
-        ++cntx[a[i]];
-        ++cnty[b[i]];
-        if(mx.empty() or cntx[a[i]] > cntx[mx.back()]){
-            mx.clear();
-            mx.pub(a[i]);
-        }else if(cntx[a[i]] == cntx[mx.back()]){
-            mx.pub(a[i]);
-        }
-        if(my.empty() or cnty[b[i]] > cnty[my.back()]){
-            my.clear();
-            my.pub(b[i]);
-        }else if(cnty[b[i]] == cnty[my.back()]){
-            my.pub(b[i]);
-        }
-    }
-    sort(all(mx));
-    sort(all(my));
-    int max_x = mx[(len(mx) + 1) / 2 - 1];
-    int max_y = my[(len(my) + 1) / 2 - 1];
-    int res = oo;
-    {
-        int ans = 0;
-        fo(i, 1, n) ans += abs(a[i] - max_x);
-        int c[n+5];
-        fo(i, 1, n) c[i] = b[i];
-        sort(c+1, c+n+1);
-        int mid = (n + 1) / 2;
-        fo(i, 1, n){
-            if(i < mid){
-                ans += abs(c[i] - (c[mid] - (mid - i)));
-            }else if(i > mid){
-                ans += abs(c[i] - (c[mid] + (i - mid)));
-            }
-        }
-        // cout << ans, el;
-        res = min(res, ans);
-    }
-    {
-        int ans = 0;
-        fo(i, 1, n) ans += abs(b[i] - max_y);
-        int c[n+5];
-        fo(i, 1, n) c[i] = a[i];
-        sort(c+1, c+n+1);
-        int mid = (n + 1) / 2;
-        fo(i, 1, n){
-            if(i < mid){
-                ans += abs(c[i] - (c[mid] - (mid - i)));
-            }else if(i > mid){
-                ans += abs(c[i] - (c[mid] + (i - mid)));
-            }
-        }
-        // cout << ans, el;
-        res = min(res, ans);
-    }
-    cout << res;
+    cout << min(cal1(a, n), cal2(b, n));
 }
 
 signed main(){
-    ios::sync_with_stdio(false); cin.tie(nullptr);
+    cin.tie(nullptr) -> sync_with_stdio(false);
     
-    signed orz = 1; //cin >> orz;
-    while(orz --> 0){
+    signed hehe = 1; if(false) cin >> hehe;
+    for(signed hihi = 1; hihi <= hehe; ++hihi){
+        print(hihi);
         LonggVuz();
-        if(orz) el;
     }
     
-    cerr << "Execution Time: " << 0.001 * clock() << "s\n";
+    cerr << "Execution Time: " << clock() << "ms\n";
 }
