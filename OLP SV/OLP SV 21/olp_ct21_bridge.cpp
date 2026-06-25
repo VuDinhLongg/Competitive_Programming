30f15f5506933bc37b0d46a7f93af49f3e159f66
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-06-25 20:55:48 +0700

    Create olp_ct21_bridge.cpp

---
+++ OLP SV/OLP SV 21/olp_ct21_bridge.cpp
@@ -0,0 +1,48 @@
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
+
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+
+const int mod = 1e9 + 7;
+
+inline void LonggVuz(){
+    string s, t; cin >> s >> t;
+    int n = len(s);
+    int res = 1;
+    rep(i, n){
+    	if(s[i] == t[i]){
+    		if(s[i] == 'X') res = res * 2 % mod;
+    		else res = res * 0 % mod;
+    	}
+    }
+    cout << res, el;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    signed o = 1; if(1) cin >> o;
+    rep(i, o) orz(i), LonggVuz();
+    
+    cerr << "[exec time = `" << clock() << "ms`]";
+}
