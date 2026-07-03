0fa6536ce9e0ce2e45e3dc7fd7b6ccaa0371b15c
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-04 01:47:56 +0700

    Create icpc22_mt_i.cpp

---
+++ ICPC/ICPC vòng Miền/ICPC miền Trung/icpc22_mt_i.cpp
@@ -0,0 +1,50 @@
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
+
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+
+int n, a[mxn], dp[4][mxn][5];
+
+inline void LonggVuz(){
+	cin >> n;
+	fo(i, 1, n) cin >> a[i];
+	dp[0][0][0] = 1;
+	fo(i, 1, n){
+		a[i] %= 5;
+		fd(j, 3, 0) rep(r, 5){
+			int nr = (r + a[i]) % 5;
+			dp[j][i][nr] += dp[j][i - 1][nr];
+			if(j) dp[j][i][nr] += dp[j - 1][i - 1][r];
+		}
+	}
+	cout << dp[3][n][0];
+}
+
+signed main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	signed o = 1; if(false) cin >> o;
+	rep(x, o) orz(x), LonggVuz();
+	
+	cerr << "[exec time = `" << clock() << "ms`]";
+}
