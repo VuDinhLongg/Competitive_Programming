505bf81916cc9c02ff296b3fd8ebb8faefaba0c1
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-05 20:52:52 +0700

    Create icpc23_mn_b.cpp

---
+++ ICPC/ICPC vòng Miền/ICPC miền Nam/icpc23_mn_b.cpp
@@ -0,0 +1,57 @@
+// LonggVuz
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x))
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define dub double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+inline void LonggVuz(){
+    int n, k; cin >> n >> k;
+    string s = "";
+    fd(i, 9, 2){
+        while(n % i == 0){
+            s.pub(i + '0');
+            n /= i;
+        }
+    }
+    if(n > 1) out(-1);
+    if(len(s) > k) out(-1);
+    while(len(s) < k) s.pub('1');
+    reverse(all(s));
+    cout << s;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
