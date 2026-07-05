ba19ebed0041740a4331f8ef33be76eda94d8928
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-05 22:10:51 +0700

    Create icpc23_regional_l.cpp

---
+++ ICPC/ICPC vòng Quốc Gia/icpc23_regional_l.cpp
@@ -0,0 +1,51 @@
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
+
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+
+
+
+inline void LonggVuz(){
+	int n; cin >> n;
+	string s; cin >> s;
+	if(n == 1) out("YES");
+	if(len(s) < n) out("NO");
+	reverse(all(s));
+	fd(i, len(s) - 1, 0) if(s[i] == '1'){
+		if(i > n - 1) out("YES");
+		if(i == n - 1){
+			fd(j, i - 1, 0) if(s[j] == '1') out("YES");
+		}
+		out("NO");
+	}
+	out("NO");
+}
+
+signed main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	signed o = 1; if(1) cin >> o;
+	rep(x, o) orz(x), LonggVuz();
+	
+	cerr << "[exec time = `" << clock() << "ms`]";
+}
