d3a61466893211f42c01ec3939d5ed5d8acc4c7c
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-05 20:53:52 +0700

    Create icpc23_mn_h.cpp

---
+++ ICPC/ICPC vòng Miền/ICPC miền Nam/icpc23_mn_h.cpp
@@ -0,0 +1,52 @@
+// LonggVuz
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <LonggVuz.h>
+#else
+#define debug(...)
+#endif
+
+#define el cout << '\n'
+#define inc(i, a, b, c) for(int32_t i=a; i<=b; i+=c)
+#define dec(i, a, b, c) for(int32_t i=a; i>=b; i-=c)
+#define fo(i, a, b) for(int32_t i=a; i<=b; i++)
+#define fd(i, a, b) for(int32_t i=a; i>=b; i--)
+#define out(x) return void(cout << (x))
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define vec vector
+#define pub push_back
+#define pob pop_back
+#define dub double
+#define int int64_t
+
+const int mod = 1e9 + 7;
+const int oo = 1e18 + 7;
+const int mxn = 1e6 + 7;
+
+
+
+inline void LonggVuz(){
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    if(a % b == 0 or d % c == 0) out(-1);
+    int res = a;
+    while(res <= c){
+        if(res % b and d % res and c % res == 0) out(res);
+        res += a;
+    }
+    cout << -1;
+}
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    signed orz = 1; if(false) cin >> orz;
+    while(orz --> 0){
+        LonggVuz();
+        if(orz) el;
+    }
+    
+    cerr << "Execution Time: " << clock() << "ms\n";
+}
