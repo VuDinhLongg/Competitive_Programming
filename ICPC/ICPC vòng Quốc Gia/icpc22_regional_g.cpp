709d3f9991930f5eceb05f0443611eb3d479d1be
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-05 21:06:45 +0700

    Create icpc22_regional_g.cpp

---
+++ ICPC/ICPC vòng Quốc Gia/icpc22_regional_g.cpp
@@ -0,0 +1,48 @@
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
+
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+
+int n, r, x1, x2, p[mxn];
+
+inline void LonggVuz(){
+	cin >> n >> r >> x1 >> x2;
+	fo(i, 1, n) cin >> p[i];
+	fo(i, 1, n){
+		if(x1 < x2){
+			if(p[i] + r <= x1) out("GOAL");
+		}else{
+			if(p[i] - r >= x1) out("GOAL");
+		}
+	}
+	out("NO GOAL");
+}
+
+signed main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	signed o = 1; if(false) cin >> o;
+	rep(x, o) orz(x), LonggVuz();
+	
+	cerr << "[exec time = `" << clock() << "ms`]";
+}
