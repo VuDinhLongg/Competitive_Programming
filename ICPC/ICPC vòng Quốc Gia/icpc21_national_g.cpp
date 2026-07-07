1af86df1da184fc206393632bbc6a9cf83689a60
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-07 14:21:10 +0700

    Create icpc21_national_g.cpp

---
+++ ICPC/ICPC vòng Quốc Gia/icpc21_national_g.cpp
@@ -0,0 +1,46 @@
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
+
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+
+int n, k, a[1005];
+
+inline void LonggVuz(){
+	cin >> n >> k;
+	fo(i, 1, n) cin >> a[i];
+	sort(a+1, a+n+1);
+	int minn = 0;
+	fo(i, 1, k) if(a[i] > 1) minn += a[i];
+	int maxx = 0;
+	fo(i, n - k + 1, n) if(a[i] > 1) maxx += a[i];
+	cout << minn << ' ' << maxx;
+}
+
+signed main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	signed o = 1; if(false) cin >> o;
+	rep(x, o) orz(x), LonggVuz();
+	
+	cerr << "[exec time = `" << clock() << "ms`]";
+}
