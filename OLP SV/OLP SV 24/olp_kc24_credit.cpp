88a1c31ccd338a4682a6511aaabf7d80f8c6145b
Vũ Đình Long <160626269+VuDinhLongg@users.noreply.github.com>
2026-07-14 17:49:25 +0700

    Update olp_kc24_credit.cpp

--- OLP SV/OLP SV 24/olp_kc24_credit.cpp
+++ OLP SV/OLP SV 24/olp_kc24_credit.cpp
@@ -1,89 +1,68 @@
-/*
-    vudinhlong 1:17:41 sa, 16/12/2024
-    => Old style, update later... :D
-*/
-
-// LonggVuz
-#include<bits/stdc++.h>
+/*======================
+   Author : @LonggVuz
+======================*/
+#include <bits/stdc++.h>
 using namespace std;
-// ☞ Think twice, code once ☜
-void End(){
-    cerr << "=> Smoothly Finished! Time taken: ";
-    cerr << (0.001 * clock()) << "s" << string(25, '\t');
-}
-#define Sonic() cin.tie(0) -> sync_with_stdio(0)
-#define out(x) return cout << x, void()
-#define fix(x) fixed << setprecision(x)
-#define all(x) x.begin(), x.end()
-#define len(x) (int)x.size()
-#define pb push_back
-#define is insert
-#define el '\n'
-#define ld long double
-#define int int64_t
 
-const int mod = 1e9 + 7;
-const int oo = 1e18 + 7;
-const int mxn = 2e5 + 7;
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(...) 1608
+#define orz(...) 2005
+#endif
 
-#define pe pair<int, int>
+#define el cout << '\n'
+#define all(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+#define out(x) return void(cout << (x) << '\n')
+#define rep(i, n) for(int32_t i=0; i<n; ++i)
+#define fo(i, a, b) for(int32_t i=a; i<=b; ++i)
+#define fd(i, a, b) for(int32_t i=a; i>=b; --i)
+#define maxi(a, b) ((a) < (b) ? (a = b, 1) : 0)
+#define mini(a, b) ((a) > (b) ? (a = b, 1) : 0)
+#define int long long
+#define mxn 1'000'007
+#define ai3 array<int, 3>
+#define ai4 array<int, 4>
+#define pii pair<int, int>
 #define fi first
 #define se second
-int n, c, d[mxn], t[mxn], f[mxn * 4];
-pe a[mxn];
 
-void build(int id, int l, int r){
-	if(l == r){
-		f[id] = d[l]; return;
-	}
-	int m = (l + r) / 2;
-	build(id * 2, l, m);
-	build(id * 2 + 1, m + 1, r);
-	f[id] = max(f[id * 2], f[id * 2 + 1]);
-}
+int n, c;
+pii a[mxn];
 
-int get(int id, int l, int r, int u, int v){
-	if(r < u or v < l) return -oo;
-	if(u <= l and r <= v) return f[id];
-	int m = (l + r) / 2;
-	return max(get(id * 2, l, m, u, v), get(id * 2 + 1, m + 1, r, u, v));
-}
-
-void LonggVuz(){
-	cin >> n >> c;
-	for(int i=1; i<=n; i++){
-		cin >> a[i].fi;
-	}
-	for(int i=1; i<=n; i++){
-		cin >> a[i].se;
-	}
-	sort(a+1, a+n+1);
-	for(int i=1; i<=n; i++){
-		if(i < n) d[i] = a[i + 1].fi - a[i].fi;
-		t[i] = t[i - 1] + a[i].se;
-	}
-	build(1, 1, n - 1);
-	int res = oo;
-	for(int i=1; i<=n; i++){
-		int j = lower_bound(t+i, t+n+1, t[i - 1] + c) - t;
-		if(j == n + 1) break;
-		if(j == i){
-			res = min(res, d[i]);
-			continue;
+bool check(int mid){
+	int res = a[1].se;
+	fo(i, 2, n){
+	    if(res >= c) return 1;
+		if(a[i].fi - a[i - 1].fi <= mid){
+			res += a[i].se;
+		}else{
+			res = a[i].se;
 		}
-		int mx = get(1, 1, n - 1, i, j - 1);
-		if(mx > -oo) res = min(res, mx);
-	}cout << res;
+	}
+	return res >= c;
+}
+
+inline void LonggVuz(){
+	cin >> n >> c;
+	fo(i, 1, n) cin >> a[i].fi;
+	fo(i, 1, n) cin >> a[i].se;
+	sort(a+1, a+n+1);
+	int l = -1, r = 1e9;
+	while(r - l > 1){
+		int mid = l + r >> 1;
+		if(check(mid)) r = mid;
+		else l = mid;
+	}
+	cout << r;
 }
 
 signed main(){
-	Sonic();
+	ios::sync_with_stdio(false); cin.tie(nullptr);
 	
-	signed test = 1;
-	//cin >> test;
-	for(signed i=1; i<=test; i++){
-		LonggVuz();
-	}
+	signed o = 1; if(false) cin >> o;
+	rep(x, o) orz(x), LonggVuz();
 	
-	End();
+	cerr << "[exec time = `" << clock() << "ms`]";
 }
